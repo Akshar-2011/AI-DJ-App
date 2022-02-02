7a3ff38a1d9ca5b658d36e7b9cc746e14d5b99f2
Akshar-2011 <84450951+Akshar-2011@users.noreply.github.com>
2022-02-02 19:36:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X = "+leftWristX+" Left Wrist Y = "+leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X = "+rightWristX+" Right Wrist Y = "+rightWristY);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("Right wrist score = "+scoreRightWrist+" Left wrist score = "+scoreLeftWrist);
+        console.log(scoreLeftWrist);
+    }
+}
+function modelLoaded(){
+    console.log("PoseNet is Initialized");
+}
+function draw(){
+    image(video, 0, 0, 600, 500);
+    fill('Red');
+    stroke('Red');
+    if(scoreLeftWrist > 0.2){
+    circle(leftWristX, leftWristY, 20);
+    inNumberLeftWristY = Number(leftWristY);
+    remove_decimal = floor(inNumberLeftWristY);
+    volume = remove_decimal/500;
+    document.getElementById("volume").innerHTML = "Volume = "+volume;
+    song.setVolume(volume);
+  }
+  circle(rightWristX, rightWristY, 20);
+  if(scoreRightWrist > 0.2){
+    if(rightWristY > 0 && rightWristY <= 100){
+        document.getElementById("speed").innerHTML = "Speed = 0.5x";
+        song.rate(0.5);
+    }
+    else if(rightWristY > 100 && rightWristY <= 200){
+        document.getElementById("speed").innerHTML = "Speed = 1x";
+        song.rate(1);
+    }
+    else if(rightWristY > 200 && rightWristY <= 300){
+        document.getElementById("speed").innerHTML = "Speed = 1.5x";
+        song.rate(1.5);
+    }
+    else if(rightWristY > 300 && rightWristY <= 400){
+        document.getElementById("speed").innerHTML = "Speed = 2x";
+        song.rate(2);
+    }
+    else if(rightWristY > 400 && rightWristY <= 500){
+        document.getElementById("speed").innerHTML = "Speed = 2.5x";
+        song.rate(2.5);
+    }
+  }
+}
+function preload(){
+    song = loadSound("music.mp3");
+}
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+function stop(){
+    song.stop();
+}
